0a29faa1ad9dbfe7b1ad9874859e668049de895a
richardt22 <richardtrinh22@gmail.com>
2018-11-21 22:05:12 -0500

    background image2

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,14 +49,14 @@
 
 				$bg
 					.removeClass('fixed')
-//					.css('transform', 'matrix(1,0,0,1,0,0)');
+					.css('transform', 'matrix(1,0,0,1,0,0)');
 
 				$window
 					.on('scroll._parallax', function() {
 
 						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
-//						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
 
 					});
 
