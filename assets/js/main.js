18fee6a1a1831a19f383f8834d30ba54d35cbfaf
richardt22 <richardtrinh22@gmail.com>
2018-11-21 21:13:27 -0500

    disable parallax

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,16 +47,16 @@
 
 			on = function() {
 
-				$bg
-					.removeClass('fixed')
-					.css('transform', 'matrix(1,0,0,1,0,0)');
+//				$bg
+//					.removeClass('fixed')
+//					.css('transform', 'matrix(1,0,0,1,0,0)');
 
 				$window
 					.on('scroll._parallax', function() {
 
 						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+//						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
 
 					});
 
